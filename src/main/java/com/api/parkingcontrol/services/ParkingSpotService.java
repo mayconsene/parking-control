9741e8ef1b5b8b4fa216019a48a068eeb5eafce1
mayconsene <mayconmoraessene@gmail.com>
2022-06-28 19:13:42 -0300

    28-06-2022

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,7 +1,10 @@
 package com.api.parkingcontrol.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 
 @Service
@@ -11,6 +14,11 @@ public class ParkingSpotService {
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
     }
     
 }
